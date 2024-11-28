507677b4ccb2f2a33043c1262d2284cf7db7b084
Daniloasgit <danilo.a.carvalho6@aluno.senai.br>
2024-11-28 10:55:04 -0300

    membro novo

--- javascript.js
+++ javascript.js
@@ -1,11 +1,50 @@
 
 
 /*Modal para os botões login e registrar*/
-document.querySelector("#show-login").addEventListener("click", function(){
-    document.querySelector(".popup").classList.add("active");
-});
+function ShowPopup()  {
+    document.getElementById('show-login').style.display = 'block';
+};
 
 
+/*Modal para os botões login e registrar*/
+
+function ClosePopup () { 
+    document.getElementById('popup').style.display = 'none';
+};
+
+
+window.onclick = function(event) {
+    const modal = document.getElementById('show-login');
+    if (event.target === modal) {
+        ClosePopup();
+    }
+};
+
+
+
+// const modal = document.getElementById('area');
+
+// const logar = document.getElementById('button');
+
+// const cancelar = document.getElementsByClassName('botao-cancelar')[0];
+
+// logar.onclick = function() {
+//     modal.style.display = 'block'
+// };
+
+//     cancelar.onclick = function(){
+//         modal.style.display = 'none'
+//     };
+
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = 'none'
+//         };
+//     };
+
+// Slider de imagens
+
+ 
 var count = 1;
 
 document.getElementById("radio1").checked = true;
@@ -24,3 +63,9 @@ if(count>4){
 document.getElementById("radio"+count).checked = true;
 
 }
+
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+      $("#myModal").modal();
+    });
+  });
